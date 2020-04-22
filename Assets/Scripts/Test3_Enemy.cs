95757a452d75e03a19e01e06bbcf9b2be42e4079
saheen1998 <47032114+saheen1998@users.noreply.github.com>
2020-04-22 14:16:31 -0500

    22 April 2020

---
+++ Assets/Scripts/Test3_Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test3_Enemy : MonoBehaviour
+{
+    private float dirX;
+    private float dirY;
+    private float dirZ;
+    void Start()
+    {
+        dirX =  Random.Range(1f, 3f);
+        dirY =  Random.Range(1f, 3f);
+        dirZ =  Random.Range(1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(dirX, dirY, dirZ);
+    }
+}
--- Assets/Scripts/Test3_Obstacle.cs
+++ Assets/Scripts/Test3_Obstacle.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Test3_Obstacle : MonoBehaviour
 {
     public GameObject particleSystem;
+
+    void Update() {
+        transform.Rotate(0, 1, 0);
+    }
+
     void OnDestroy() {
         Instantiate(particleSystem, transform.position, Quaternion.identity);
     }
